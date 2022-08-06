f60516990cf50bf786b04240d1729e43c431d132
Alex <cnalex_2@hotmail.com>
2022-08-05 22:34:50 -0500

    subida

---
+++ src/component/publicidad/Publicidad.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Publicidad = () => {
+  return (
+    <div style={{background: " #005aa3 ", textAlign:"center", minHeight:'8vh'}}>
+        <h2 style={{color:'white'}}>Felices fiestas patrias</h2>
+    </div>
+  )
+}
+
+export default Publicidad
